d96edcd503601d743d57f0f95f89d5d2ced6cdff
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-12-29 19:58:56 +0100

    add all code in one for frontend

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('info'))
+app.get('/', (req, res) => res.render('welcome'))
 app.get('/app', (req, res) => res.render('index'))
 
+app.get('/about',function(req,res) {
+  res.sendFile('about.html');
+});
 
 io.on('connection', (socket) => {
   socket.on('event', function(msg){
@@ -27,4 +30,4 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(PORT, () => console.log(`Hey Hey and welcome again Listening on ${ PORT }`))
+server.listen(PORT, () => console.log(`Hey Hey and welocme again Listening on ${ PORT }`))
